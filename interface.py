b5d8b5effc5d4d71a1c1b84602a5de3d569efd0c
loneWolf 01 <bhupi2210uk@gmail.com>
2022-12-07 19:45:56 +0530

    file Interface add

---
+++ interface.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, render_template, redirect, request, url_for
+import config1
+from utils import MedicalInsurence
+from tree import addition
+app = Flask(__name__)
+
+
+@app.route("/jkl")
+def aabb():
+    return addition()
+
+@app.route('/predict',methods = ['GET', "POST"])
+def prediction():
+    if request.method == 'POST':
+        data = request.form
+        
+        print('data :',data)
+
+        med_ins = MedicalInsurence(data)
+        price = med_ins.get_predicted_price()
+        print("::::::::::",price)
+        return jsonify({"Price :":price})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=config1.PORT_NUMBER)
